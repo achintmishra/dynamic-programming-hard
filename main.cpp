b06d9d71ed8e34493fcffd11b7ef972400fbcdbd
Achint Mishra <>
2021-05-25 12:43:04 +0000

    longest common subs

--- main.cpp
+++ main.cpp
@@ -60,6 +60,63 @@ int mincostpathmemo(int**input,int m,int n)
   return mincostpathhelpermemo(input,m,n,0,0,output);
 }
 
+int longestcommonsubsequence(string x,string y)
+{
+  if(x.size()==0 || y.size()==0)
+  {
+    return 0;
+  }
+  if(x[0]==y[0])
+  {
+    return 1+longestcommonsubsequence(x.substr(1),y.substr(1));
+  }
+  else
+  {
+    int a = longestcommonsubsequence(x.substr(1),y);
+    int b = longestcommonsubsequence(x,y.substr(1));
+    int c = longestcommonsubsequence(x.substr(1),y.substr(1));
+    return max(a,max(b,c));
+  }
+}
+
+int lcsmemhelper(string x,string y,int **output)
+{
+    if(x.size()==0 || y.size()==0)
+  {
+    return 0;
+  }
+  if(output[x.size()][y.size()]!=-1)
+  {
+    return output[x.size()][y.size()];
+  }
+  if(x[0]==y[0])
+  {
+    return 1+longestcommonsubsequence(x.substr(1),y.substr(1));
+  }
+  else
+  {
+    int a = longestcommonsubsequence(x.substr(1),y);
+    int b = longestcommonsubsequence(x,y.substr(1));
+    int c = longestcommonsubsequence(x.substr(1),y.substr(1));
+    int ans= max(a,max(b,c));
+    output[x.size()][y.size()]=ans;
+    return ans;
+  }
+}
+
+int lcsmem(string x,string y)
+{
+  int **output=new int*[x.size()+1];
+  for(int i=0;i<=x.size();i++)
+  {
+    output[i]=new int(y.size()+1);
+    for(int j=0;j<=y.size();j++)
+    {
+      output[i][j]=-1;
+    }
+  }
+  return lcsmemhelper(x,y,output);
+}
 int main() 
 {
   int m,n;
@@ -74,4 +131,5 @@ int main()
     }
   }
   cout<<mincostpathmemo(input,m,n)<<endl;
+  cout<<lcsmem("bcde", "amce")<<endl;
 }
